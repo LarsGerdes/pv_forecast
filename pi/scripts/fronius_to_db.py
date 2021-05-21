34e3275ccff6aff063f7f9286c2f14f3b021e8e6
Lars <lars.gerdes95@gmail.com>
2021-05-21 23:27:52 +0200

    Error handling

--- pi/scripts/fronius_to_db.py
+++ pi/scripts/fronius_to_db.py
@@ -22,29 +22,32 @@ dic = get(url)
 dic = dic.json()
 
 # Convert string to UTC datetime.
-t = dic['Head']['Timestamp']
-t = datetime.fromisoformat(t)
-t = t.astimezone(timezone.utc)
-
-# Connect to database.
-conf = conf['database']
-conn = connect(dbname=conf['name'],
-               user=conf['user'],
-               password=conf['password'],
-               host=conf['host'],
-               port=conf['port'])
-cur = conn.cursor()
+dt = dic['Head']['Timestamp']
+dt = datetime.fromisoformat(dt)
+dt = dt.astimezone(timezone.utc)
 
 try:
-    # Execute SQL statement.
-    cur.execute(
-        'insert into energy_minute values (%s, %s)',
-        (t, dic['Body']['Data']['TOTAL_ENERGY']['Value'])
-    )
-    conn.commit()
-except KeyError:
-    print(dic)
+    wh = dic['Body']['Data']['TOTAL_ENERGY']['Value']
 
-# Close database connection.
-cur.close()
-conn.close()
+    # Connect to database.
+    conf = conf['database']
+    conn = connect(dbname=conf['name'],
+                   user=conf['user'],
+                   password=conf['password'],
+                   host=conf['host'],
+                   port=conf['port'])
+    cur = conn.cursor()
+
+    # Execute SQL statement.
+    cur.execute('insert into energy_minute values (%s, %s)', (dt, wh))
+    conn.commit()
+
+    # Close database connection.
+    cur.close()
+    conn.close()
+
+except KeyError:
+    # Sometimes the API is not accessible. However, this does not happen very
+    # often. Therefore, the error message is simply suppressed.
+    pass
+
