53d1bce374bf49bc3e38bc8049baafa4e6d950e4
Lars <lars.gerdes95@gmail.com>
2021-05-22 14:33:36 +0200

    datetime to string

--- pi/scripts/fronius_to_db.py
+++ pi/scripts/fronius_to_db.py
@@ -4,7 +4,6 @@
 # Libraries
 from toml import load                       # loading config file
 from requests import get                    # API connection
-from datetime import datetime, timezone     # timestamp operations
 from psycopg2 import connect                # database connection
 
 # Load data about IP and database access.
@@ -21,21 +20,14 @@ url = url.format(conf['fronius']['ip'])
 dic = get(url)
 dic = dic.json()
 
-# Convert string to UTC datetime.
+# Select Timestamp and value of total energy
 dt = dic['Head']['Timestamp']
-dt = datetime.fromisoformat(dt)
-dt = dt.astimezone(timezone.utc)
 
 try:
     wh = dic['Body']['Data']['TOTAL_ENERGY']['Value']
 
     # Connect to database.
-    conf = conf['database']
-    conn = connect(dbname=conf['name'],
-                   user=conf['user'],
-                   password=conf['password'],
-                   host=conf['host'],
-                   port=conf['port'])
+    conn = connect(**conf['database'])
     cur = conn.cursor()
 
     # Execute SQL statement.
@@ -50,4 +42,3 @@ except KeyError:
     # Sometimes the API is not accessible. However, this does not happen very
     # often. Therefore, the error message is simply suppressed.
     pass
-
